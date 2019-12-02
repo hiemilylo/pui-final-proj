9bdb17355c11fc0544f6a957c8a7ebc199037f21
Emily Lo <hiemilylo@gmail.com>
2019-12-01 23:46:32 -0500

    GL transition effects

--- src/components/ProjectScrollList/projectscrolllist.js
+++ src/components/ProjectScrollList/projectscrolllist.js
@@ -1,53 +1,106 @@
-import React from "react";
+import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 // import appRoutes from "../../shared/appRoutes";
 // import MovieList from "../MoviesPage/movieList.js";
-
-// import logo from '../../logo.svg';
+import { LinearCopy } from "gl-react";
+import { Surface } from "gl-react-dom";
+import GLTransition from "react-gl-transition";
+import GLTransitions from "gl-transitions";
 import { Waypoint } from 'react-waypoint';
+import P1 from '../../assets/projects/p1.jpg';
+import P2 from '../../assets/projects/p2.jpg';
+import P3 from '../../assets/projects/p3.jpg';
+
 import "./projectscroll.css";
 
-const ProjectScrollList = () => {
-  const changeBckgnd = (secId) => () => {
-    let bckgrnd = document.querySelector('.section-container');
-    switch (secId) {
-      case 1:
-          bckgrnd.style['background-image'] = 'url(https://dxaurk9yhilm4.cloudfront.net/images/569/chileno_landscape_2cbd80a0345be3b5a0b7649a1a2d057f.jpg)';
-          bckgrnd.style.color = 'none';
-          break;
-      case 2:
-          bckgrnd.style['background-image'] = 'url(https://dxaurk9yhilm4.cloudfront.net/images/291/Bakers-hero_e5c6ae03864007f58832dde55a8f653b.jpg)';
-          bckgrnd.style.color = 'none';
-          break;
-      case 3:
-          bckgrnd.style['background-image'] = 'url(https://dxaurk9yhilm4.cloudfront.net/images/352/driftwood3-carousel_2cbd80a0345be3b5a0b7649a1a2d057f.jpg)';
-          bckgrnd.style.color = 'none';
-          break;
-      default:
-          bckgrnd.style.background = 'white';
-          break;
-    }
+class ProjectScrollList extends Component {
+    _didMount = false;
+    timer = null;
+    state = { progress: 0,
+              currBackground : P1,
+              nextBackground : P1};
+    flyEye = GLTransitions.find((x) => (x.name === 'flyeye'));
+
+    changeBckgnd = (secId) => () => {
+        console.log(secId);
+        let bckgrnd = document.querySelector('.section-container');
+        switch (secId) {
+            case 1:
+                // bckgrnd.style['background-image'] = 'url(https://dxaurk9yhilm4.cloudfront.net/images/569/chileno_landscape_2cbd80a0345be3b5a0b7649a1a2d057f.jpg)';
+                this.setState({nextBackground: P1});
+                bckgrnd.style.color = 'none';
+                break;
+            case 2:
+                // bckgrnd.style['background-image'] = 'url(https://dxaurk9yhilm4.cloudfront.net/images/291/Bakers-hero_e5c6ae03864007f58832dde55a8f653b.jpg)';
+                this.setState({nextBackground: P2});
+                bckgrnd.style.color = 'none';
+                break;
+            case 3:
+                // bckgrnd.style['background-image'] = 'url(https://dxaurk9yhilm4.cloudfront.net/images/352/driftwood3-carousel_2cbd80a0345be3b5a0b7649a1a2d057f.jpg)';
+                this.setState({nextBackground: P3});
+                bckgrnd.style.color = 'none';
+                break;
+            default:
+                bckgrnd.style.background = 'white';
+                break;
+        }
+        if (this._didMount &&
+            this.state.currBackground !== this.state.nextBackground) {
+            console.log('START');
+            if (this.timer) {
+                clearInterval(this.timer);
+            }
+            this.timer = setInterval(() => {
+                while (this.state.progress >= 1) {
+                    this.setState({progress: 0, currBackground: this.state.nextBackground});
+                    clearInterval(this.timer);
+                    this.timer = null;
+                    console.log('END');
+                }
+                let newProg = this.state.progress + 0.04;
+                this.setState({progress: newProg});
+              }, 20);
+        }
   }
 
-  return (
-    <div className="scroll-container">
-        <h3> Scrolling section </h3>
-        <div className="section-container">
-            <Waypoint onEnter={changeBckgnd(1)} >
-                <section id="section-1">section 1
-                </section>
-            </Waypoint>
-            <Waypoint onEnter={changeBckgnd(2)} >
-                <section id="section-1">section 2
-                </section>
-            </Waypoint>
-            <Waypoint onEnter={changeBckgnd(3)} >
-                <section id="section-1">section 3
-                </section>
-            </Waypoint>
+  componentDidMount() {
+    this._didMount = true;
+  }
+
+  render() {
+    return (
+        <div className="scroll-container">
+            <h3> Scrolling section </h3>
+            <div className="parent">
+                <Surface width={600} height={400}>
+                    {this.state.progress > 0 ?
+                    <GLTransition
+                        from={this.state.currBackground}
+                        to={this.state.nextBackground}
+                        transition={this.flyEye}
+                        progress={this.state.progress}
+                    />
+                    : <LinearCopy>{this.state.currBackground}</LinearCopy>}
+                </Surface>
+                <div className="section-container">
+                    <Waypoint onEnter={this.changeBckgnd(1)} >
+                        <section id="section-1">
+                            <h3>Section 1</h3>
+                        </section>
+                    </Waypoint>
+                    <Waypoint onEnter={this.changeBckgnd(2)} >
+                        <section id="section-1">section 2
+                        </section>
+                    </Waypoint>
+                    <Waypoint onEnter={this.changeBckgnd(3)} >
+                        <section id="section-1">section 3
+                        </section>
+                    </Waypoint>
+                </div>
+            </div>
         </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ProjectScrollList;
