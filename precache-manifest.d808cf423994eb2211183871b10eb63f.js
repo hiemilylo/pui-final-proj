72af0ce0ca7e0b29bdd360c2c01c86a098cc30f7
Emily Lo <hiemilylo@gmail.com>
2019-12-06 15:17:20 -0500

    Updates

---
+++ precache-manifest.d808cf423994eb2211183871b10eb63f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9d64ae0c5ff7b574291646a03b1c8d",
+    "url": "/pui-final-proj/index.html"
+  },
+  {
+    "revision": "ff40489c1f789c316545",
+    "url": "/pui-final-proj/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "3d04b883bca5d3c322dd",
+    "url": "/pui-final-proj/static/css/main.fd80a4f8.chunk.css"
+  },
+  {
+    "revision": "ff40489c1f789c316545",
+    "url": "/pui-final-proj/static/js/2.05118283.chunk.js"
+  },
+  {
+    "revision": "3d04b883bca5d3c322dd",
+    "url": "/pui-final-proj/static/js/main.333d30ca.chunk.js"
+  },
+  {
+    "revision": "e0d2e59fb4aacbc23178",
+    "url": "/pui-final-proj/static/js/runtime-main.8f83d6a2.js"
+  },
+  {
+    "revision": "26612236b50f34a89ed84be8af6f1ce6",
+    "url": "/pui-final-proj/static/media/1.26612236.jpg"
+  },
+  {
+    "revision": "1922a8b278d4ee9322687f102e3551e1",
+    "url": "/pui-final-proj/static/media/2.1922a8b2.jpg"
+  },
+  {
+    "revision": "b518bc2a44b187ada6885b5027532e94",
+    "url": "/pui-final-proj/static/media/p3.b518bc2a.jpg"
+  }
+]);
